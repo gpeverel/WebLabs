f96179cd03880a48c44e366e4f4a5693e758de05
KennyZwei <narbal199111111@gmail.com>
2022-03-31 17:33:00 +0400

    Laba 5 script without function

--- Laba 5/scripts/script.js
+++ Laba 5/scripts/script.js
@@ -5,6 +5,20 @@ $(function(){
      "Ответить на электронные письма", 
      "Подготовиться к лекции в понедельник", 
      "Обновить несколько новых задач", "Купить продукты" ];
-    
+    $('.action:nth-child(1)').on('click', function(){
+        $('.action').removeClass("active")
+        $('.action:nth-child(1)').addClass("active")
+        $('.works').empty()
+    })
+    $('.action:nth-child(2)').on('click', function(){
+        $('.action').removeClass("active")
+        $('.action:nth-child(2)').addClass("active")
+        $('.works').empty()
+    })
+    $('.action:nth-child(3)').on('click', function(){
+        $('.action').removeClass("active")
+        $('.action:nth-child(3)').addClass("active")
+        $('.works').empty()
+    })
 
 })
