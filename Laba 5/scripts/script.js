e73d4d4db6f2eaa5da3b26aeb645fe4d89ec4c79
KennyZwei <narbal199111111@gmail.com>
2022-03-31 17:49:08 +0400

    Laba 5 todos array show

--- Laba 5/scripts/script.js
+++ Laba 5/scripts/script.js
@@ -5,13 +5,37 @@ $(function(){
      "Ответить на электронные письма", 
      "Подготовиться к лекции в понедельник", 
      "Обновить несколько новых задач", "Купить продукты" ];
+     addWorks(1)
     for(let i = 1; i <= 3;i++ ){
         const selector = '.action:nth-child(' + i + ')'
         $(selector).on('click', function(){
             $('.action').removeClass("active")
             $(this).addClass("active")
             $('.works').empty()
+            if(i !== 3){
+                addWorks(i)
+            }else{
+                openWorkPanel()
+            }
         }) 
     }
-
+    function addWorks(index){
+        let works = $('.works')
+        if(index === 2){
+            for(let i = toDos.length - 1; i > -1; i--){
+                works.append(createWork(toDos[i]))
+            }
+        }else{
+            for(let i = 0; i < toDos.length; i++){
+                works.append(createWork(toDos[i]))
+            }
+        }
+    }
+    function createWork(title){
+        const work = $('<div>',{
+            class:'work'
+        })
+        work.html(title)
+        return work
+    }
 })
