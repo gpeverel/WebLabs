1fb6336c16f1ea3fb011c8d1e722f673b810a064
KennyZwei <narbal199111111@gmail.com>
2022-04-14 20:05:20 +0400

    Laba 7 server with get method

---
+++ Laba 7/server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+let PORT = 5300
+app.use(express.json());
+
+let State =
+[ { "description" : "Купить продукты", "tags" : [ "шопинг", "рутина" ] },
+{ "description" : "Сделать несколько новых задач", "tags" : [ "писательство", "работа" ] },
+{ "description" : "Подготовиться к лекции в понедельник", "tags" : [ "работа", "преподавание" ] },
+{ "description" : "Ответить на электронные письма", "tags" : [ "работа" ] },
+ { "description" : "Вывести Грейси на прогулку в парк", "tags" : [ "рутина", "питомцы" ]},
+{ "description" : "Закончить писать книгу", "tags" : [ "писательство", "работа" ] } ]
+
+app.get('/toDos', function(req,res){
+    res.json(State)
+})
+app.listen(PORT)
