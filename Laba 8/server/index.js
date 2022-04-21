6c2f191da2773f00c8e9e50ac12dfecb84eb3968
KennyZwei <narbal199111111@gmail.com>
2022-04-21 22:50:39 +0400

    Laba 8 server ready finish

--- Laba 8/server/index.js
+++ Laba 8/server/index.js
@@ -1,27 +1,46 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
+const mongoose = require("mongoose");
 let PORT = 5300
+let toDos
 app.use(cors({
     origin: '*'
 }))
   app.use(express.json());
 
-let State =
-[ { "description" : "Купить продукты", "tags" : [ "шопинг", "рутина" ] },
-{ "description" : "Сделать несколько новых задач", "tags" : [ "писательство", "работа" ] },
-{ "description" : "Подготовиться к лекции в понедельник", "tags" : [ "работа", "преподавание" ] },
-{ "description" : "Ответить на электронные письма", "tags" : [ "работа" ] },
- { "description" : "Вывести Грейси на прогулку в парк", "tags" : [ "рутина", "питомцы" ]},
-{ "description" : "Закончить писать книгу", "tags" : [ "писательство", "работа" ] } ]
-
 app.get('/toDos', function(req,res){
-    res.json(State)
+    toDos.find({}, function(err,data){
+        if(!err){
+            res.json(data)
+        }else{
+            res.status(500).json("ERROR")
+        }
+    })
 })
 app.post('/toDos', function(req,res){
     const {tags, description} = req.body
-    State.push({tags, description})
-    console.log(State)
-    res.json("SUCCESS")
+    let newToDo = new toDos({
+        description, tags
+    })
+    newToDo.save(function(err,result){
+        if(!err){
+            res.json("SUCCESS")
+        }else{
+            res.status(500).json("ERROR")
+        }
+    })
 })
-app.listen(PORT)
+async function main() {
+    mongoose.connect('mongodb+srv://Kenny:Yfhbvfy123@booksdb.8cazt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => {
+        console.log("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSsss")
+        let toDosSchema = mongoose.Schema({
+            description:String,
+            tags:[String],
+        })
+        toDos = mongoose.model("toDos", toDosSchema)
+    }).catch(err => console.log( err ));
+
+    app.listen(PORT)
+}
+main();
